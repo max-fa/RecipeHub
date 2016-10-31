d56e17c11478af213cfb52ce6a1d4f693feb74a5
myNameisNotThis <robert.green.blue@gmail.com>
2016-10-31 09:21:27 -0500

    Some maintenance
    Edited the .gitignore file to ignore pdo_connect.php so I don't have to
    keep cutting the credentials out every commit. init.sql now inserts one
    more recipe record.

--- db/init.sql
+++ db/init.sql
@@ -3,7 +3,8 @@ INSERT INTO users (username,password) VALUES('admin','login'),('max','login');
 INSERT INTO fooditems (name,description,username) VALUES('Bread','bready and delicious','admin'),('Brocoli','green and cuniferous,','admin');
 
 INSERT INTO recipes (title,ingredients,instructions,published,user_id) VALUES('sandwich','bread','fill the bread',true,1),
-('brocoli & cheese','brocoli and cheese','smother the brocoli in cheese',false,1);
+('brocoli & cheese','brocoli and cheese','smother the brocoli in cheese',false,1),
+('fried chicken', 'fry and chicken', 'fry the damn bird',true,2);
 
 INSERT INTO traits (name,description,username) VALUES('fatty','contains high amounts of fat per serving','admin'),
 ('cuniferous','anti-cancerous amongs other things','admin'),('bready','converts to sugar','max'),('carbohydraty','like bread but more','max'),
