13a56d6cfebee06963414fb09a7057e7bee01139
myNameisNotThis <robert.green.blue@gmail.com>
2016-11-10 22:07:12 -0600

    Changed sql files
    Init and reset .sql files have been changed to reflect absence of users
    in this application

--- db/init.sql
+++ db/init.sql
@@ -1,14 +1,13 @@
-INSERT INTO users (username,password) VALUES('admin','login'),('max','login');
 
-INSERT INTO fooditems (name,description,username) VALUES('Bread','bready and delicious','admin'),('Brocoli','green and cuniferous,','admin');
+INSERT INTO fooditems (name,description) VALUES('Bread','bready and delicious'),('Brocoli','green and cuniferous');
 
-INSERT INTO recipes (title,ingredients,instructions,published,user_id) VALUES('sandwich','bread','fill the bread',true,1),
-('brocoli & cheese','brocoli and cheese','smother the brocoli in cheese',false,1),
-('fried chicken', 'fry and chicken', 'fry the damn bird',true,2);
+INSERT INTO recipes (title,ingredients,instructions) VALUES('sandwich','bread','fill the bread'),
+('brocoli & cheese','brocoli and cheese','smother the brocoli in cheese'),
+('fried chicken', 'fry and chicken', 'fry the damn bird');
 
-INSERT INTO traits (name,description,username) VALUES('fatty','contains high amounts of fat per serving','admin'),
-('cuniferous','anti-cancerous amongs other things','admin'),('bready','converts to sugar','max'),('carbohydraty','like bread but more','max'),
-('bushy','like wheat','admin');
+INSERT INTO traits (name,description) VALUES('fatty','contains high amounts of fat per serving'),
+('cuniferous','anti-cancerous amongs other things'),('bready','converts to sugar'),('carbohydraty','like bread but more'),
+('bushy','like wheat');
 
 INSERT INTO item_traits_mappings (item_id,trait_id) VALUES(1,3),(2,2);
 
--- db/reset.sql
+++ db/reset.sql
@@ -2,9 +2,7 @@ DELETE FROM recipes;
 DELETE FROM item_traits_mappings;
 DELETE FROM fooditems;
 DELETE FROM traits;
-DELETE FROM users;
 
 ALTER SEQUENCE fooditems_id_seq RESTART WITH 1;
 ALTER SEQUENCE recipes_id_seq RESTART WITH 1;
 ALTER SEQUENCE traits_id_seq RESTART WITH 1;
-ALTER SEQUENCE users_id_seq RESTART WITH 1;
